3ca69510b8ea38a5c150eb02286b4089134d3c8c
paulyehtw <42556900+paulyehtw@users.noreply.github.com>
2019-01-10 01:32:49 +0100

    5-5 upload (passed)

--- Week 5/lcs2.cpp
+++ Week 5/lcs2.cpp
@@ -5,15 +5,16 @@
 using namespace std;
 
 int lcs2(vector<int> &a, vector<int> &b) {
-  int ED_matrix[a.size()+1][b.size()+1];
+  int L_matrix[a.size()+1][b.size()+1];
+  int i,j;
   for(j = 0; j <= b.size(); j++){
     for(i = 0; i <= a.size(); i++){
-      if(i == 0 || j == 0){ED_matrix[i][j] = 0;}
-      else if(a[i-1] == b[j-1]){ ED_matrix[i][j] = ED_matrix[i-1][j-1] + 1; }
-      else{ ED_matrix[i][j] = max( ED_matrix[i-1][j], ED_matrix[i][j-1]); }
+      if(i == 0 || j == 0){L_matrix[i][j] = 0;}
+      else if(a[i-1] == b[j-1]){ L_matrix[i][j] = L_matrix[i-1][j-1] + 1; }
+      else{ L_matrix[i][j] = max( L_matrix[i-1][j], L_matrix[i][j-1]); }
     }
   }
-  return ED_matrix[a.size()][b.size()];
+  return L_matrix[a.size()][b.size()];
 }
 
 int main() {
