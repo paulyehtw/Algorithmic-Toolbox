3ef1bc9bfe43234adea455b8351d7b3afc9eb94f
paulyehtw <42556900+paulyehtw@users.noreply.github.com>
2019-01-10 01:07:11 +0100

    5-4 upload(passed)

--- Week 5/lcs2.cpp
+++ Week 5/lcs2.cpp
@@ -6,72 +6,14 @@ using namespace std;
 
 int lcs2(vector<int> &a, vector<int> &b) {
   int ED_matrix[a.size()+1][b.size()+1];
-  int i, j, insertion, deletion, match, mismatch;
-  int lcs = 0;
-  for(i = 0; i <= a.size(); i++){ED_matrix[i][0] = i;}
-  for(j = 0; j <= b.size(); j++){ED_matrix[0][j] = j;}
-  for(j = 1; j <= b.size(); j++){
-    for(i = 1; i <= a.size(); i++){
-      insertion = ED_matrix[i][j-1] + 1;
-      deletion = ED_matrix[i-1][j] + 1;
-      match = ED_matrix[i-1][j-1];
-      mismatch = ED_matrix[i-1][j-1] + 1;
-      if(a[i-1] == b[j-1]){ ED_matrix[i][j] = min( min(insertion, deletion), match); }
-      else{ ED_matrix[i][j] = min( min(insertion, deletion), mismatch); }
+  for(j = 0; j <= b.size(); j++){
+    for(i = 0; i <= a.size(); i++){
+      if(i == 0 || j == 0){ED_matrix[i][j] = 0;}
+      else if(a[i-1] == b[j-1]){ ED_matrix[i][j] = ED_matrix[i-1][j-1] + 1; }
+      else{ ED_matrix[i][j] = max( ED_matrix[i-1][j], ED_matrix[i][j-1]); }
     }
   }
-/*   for(i = 0; i <= a.size(); i++){
-    for(j = 0; j <= b.size(); j++){
-      cout<<ED_matrix[i][j]<<" ";
-    }
-    cout<<endl;
-  } */
-  i--;
-  j--;
-/*   cout<<"before while loop i, j is "<<i<<" ,"<<j<<endl;*/
-  if(a[i-1] == b[j-1]){lcs++;/* cout<<"same element at "<<i<<" ,"<<j<<endl; */}
-  while(i > 0 && j > 0){
-    /* cout<<"i, j is "<<i<<" ,"<<j<<endl; */
-    if(ED_matrix[i][j] == ED_matrix[i-1][j] + 1){ i-- ; }
-    else if(ED_matrix[i][j] == ED_matrix[i][j-1] + 1){ j-- ; }
-    else{i--; j--;}
-    if(a[i-1] == b[j-1] && i > 0 && j > 0){lcs++;/* cout<<"same element at "<<i<<" ,"<<j<<endl; */}
-  }
-  int ab_min = min( a.size(), b.size());
-  lcs = min(ab_min, lcs);
-  return lcs;
-}
-
-int lcs2_naive(vector<int> &a, vector<int> &b) {
-  vector<int> s;
-  vector<int> l;
-  int i;
-  if(a.size() > b.size()){
-    s = b;
-    l = a;
-    }
-  else{
-    s = a;
-    l = b;
-    }
-  int lcs = 0;
-  int idx = 0;
-  while(idx < l.size() && s.size() > 0){
-    //cout<<"s size is "<<s.size()<<endl;
-    for(i = idx; i < l.size();i++){
-      if(l[i] == s[0]){
-        lcs++;
-        s.erase(s.begin());
-        idx = i + 1;
-/*         cout<<"same"<<endl;
-        cout<<"s size is "<<s.size()<<endl;
-        cout<<"idx now is "<<idx<<endl; */
-        break;
-      }
-    }
-    if(i == l.size() && l[i] != s[0]){s.erase(s.begin());/* cout<<"no match"<<endl;cout<<"s size is "<<s.size()<<endl<<endl; */}
-  }
-  return lcs;
+  return ED_matrix[a.size()][b.size()];
 }
 
 int main() {
@@ -87,6 +29,5 @@ int main() {
   for (size_t i = 0; i < m; i++) {
     std::cin >> b[i];
   }
-  //std::cout << lcs2(a, b) << std::endl;
-  std::cout << lcs2_naive(a, b) << std::endl;
+  std::cout << lcs2(a, b) << std::endl;
 }
